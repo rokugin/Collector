60ec30116da3e9e601f4099bc5bc1bb84d7b6930
rokugin <rokugin@gmail.com>
2025-03-23 13:24:58 -0700

    Implement custom field rather than specific quests being untimed, Fix grammar for quest info (thanks tia)

--- Patches/SpecialOrderPatch.cs
+++ Patches/SpecialOrderPatch.cs
@@ -6,23 +6,35 @@ namespace Collector.Patches;
 public static class SpecialOrderPatch {
 
     public static void IsTimedQuest_Postfix(ref bool __result, SpecialOrder __instance) {
-        if (__instance.questKey.Value == "rokugin.collectorcp_Maru_0") { 
+        if (__instance.GetData().CustomFields.ContainsKey("rokugin.collector_untimed_order")) {
             __result = false;
         }
+        //if (__instance.questKey.Value == "rokugin.collectorcp_Maru_0") { 
+        //    __result = false;
+        //}
     }
 
     public static void GetDaysLeft_Postfix(ref int __result, SpecialOrder __instance) {
-        if (__instance.questKey.Value == "rokugin.collectorcp_Maru_0") {
+        if (__instance.GetData().CustomFields.ContainsKey("rokugin.collector_untimed_order")) {
             __result = 100;
         }
+        //if (__instance.questKey.Value == "rokugin.collectorcp_Maru_0") {
+        //    __result = 100;
+        //}
     }
 
     public static void Update_Postfix(SpecialOrder __instance) {
         if (!Game1.player.mailReceived.Contains("rokugin.collectorcp_Maru_0_PrizeTicketDeferred")
-            && __instance.questKey.Value == "rokugin.collectorcp_Maru_0" && __instance.questState.Value == SpecialOrderStatus.Complete) {
-            Game1.player.stats.Decrement("specialOrderPrizeTickets");
-            Game1.player.mailReceived.Add("rokugin.collectorcp_Maru_0_PrizeTicketDeferred");
+            && __instance.GetData().CustomFields.ContainsKey("rokugin.collector_untimed_order")
+            && __instance.questState.Value == SpecialOrderStatus.Complete) {
+                Game1.player.stats.Decrement("specialOrderPrizeTickets");
+                Game1.player.mailReceived.Add("rokugin.collectorcp_Maru_0_PrizeTicketDeferred");
         }
+        //if (!Game1.player.mailReceived.Contains("rokugin.collectorcp_Maru_0_PrizeTicketDeferred")
+        //    && __instance.questKey.Value == "rokugin.collectorcp_Maru_0" && __instance.questState.Value == SpecialOrderStatus.Complete) {
+        //    Game1.player.stats.Decrement("specialOrderPrizeTickets");
+        //    Game1.player.mailReceived.Add("rokugin.collectorcp_Maru_0_PrizeTicketDeferred");
+        //}
     }
 
 }
